30160c43573aa9b768a28eacba0caefd96122e7d
Carles Tubio <ctubio@users.noreply.github.com>
2026-08-26 02:42:22 +0200

    Updated market values without spaces.

--- src/gui/market.h
+++ src/gui/market.h
@@ -1175,7 +1175,7 @@ static void Market_PaintHeader(HWND hWnd, TsState* state) {
         { "C:", Market_Fmt(L1.prevClose), closeColor  },
         { "H:", Market_Fmt(L1.high),      highColor  },
         //{ " P:", Market_FmtQty(state->position),      textColor  },
-        { " W:", (L1.last > 0 && L1.vwap > 0) ? Market_Fmt(L1.last - L1.vwap) : "--",    vwapColor  },
+        { "W:", (L1.last > 0 && L1.vwap > 0) ? Market_Fmt(L1.last - L1.vwap) : "--",    vwapColor  },
         { "V:", formatVolume((long long)volRates.vol5min), volRates.ready ? rateColor(volRates.volRatio)  : COINS_CLR_BLUE  },  // was: formatVolume(L1.volume)
     };
 
@@ -1183,7 +1183,7 @@ static void Market_PaintHeader(HWND hWnd, TsState* state) {
     StatItem row2[] = {
         { "O:", Market_Fmt(L1.open),  openColor  },
         { "L:", Market_Fmt(L1.low),   lowColor   },
-        { " D:", bufD,                 dPnlColor  },
+        { "D:", bufD,                 dPnlColor  },
         { "U:", bufU,                 uPnlColor  },
     };
 
